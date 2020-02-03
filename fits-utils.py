e53604813ec37832419b955f6202a52ab0906053
Alex <alex.s.wheaton@gmail.com>
2020-02-03 14:55:29 +0000

    Renamed module and revised top-level docstring.

--- fits-utils.py
+++ fits-utils.py
@@ -1,15 +1,29 @@
 #! /usr/bin/env python
 
-"""Reduction program
-
-This program is used to reduce fits files so that they are ready to be used for
-science. The raw files are stored in the "dat/" folder. The program utilises the
-configparser module in order to read a "config.ini" file. This file contains a
-target ID, standard star ID, and observing bands. The program sorts the data
-into lists of specific integration times and bands. The files are then reduced.
-The program outputs reduced fits files.
-
 """
+fits-utils module
+
+This module contains various functions for the reduction, alignment and stacking
+of raw astronomical frames using the FITS file formatting standard. It is an
+extension of the astropy module, and utilises many of its basic functions to
+perform more complicated operations.
+
+The raw files are stored in the "dat/" folder, which is read-only; science
+frames are written to and read from in the "sci/" folder; temporary files
+(including master dark and flat fields) are written to and read from the "tmp/"
+directory; and stacked images are written to and read from the "sta/" directory.
+
+This module utilises the configparser module in order to generate a "config.ini"
+file. This file can be populated by the user with a target ID, standard star ID,
+and observing bands. The module can then use these parameters to parse the
+"raw/" directory and sort FITS files into lists with specific integration times
+and bands. The images can then be reduced and written out to FITS files.
+
+By convention, imported:
+
+import fits-utils as fu
+"""
+
 import math as m
 import numpy as np
 import matplotlib.pyplot as plt
