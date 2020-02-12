75d2218aa61db5ff88445b48b11d59091592da79
Alex Wheaton <alex.s.wheaton@gmail.com>
2020-02-12 12:04:10 +0000

    Reorganized some code.

--- stack.py
+++ stack.py
@@ -5,7 +5,13 @@ from fits_utils import *
 
 def main():
     for target in ["m52"]:
-        for band in ["r","g","u"]:
+        for band in ["r","g"]:
+            unaligned_images = load_fits(path="sci/", target=target, band=band)
+            aligned_images = align(unaligned_images, centroid=hybrid_centroid, filter="none")
+            stacked_image = stack(aligned_images, correct_exposure=True)
+            write_out_fits(stacked_image, "sta/{}_{}_stacked.fits".format(target, band))
+    for target in ["m52"]:
+        for band in ["u"]:
             unaligned_images = load_fits(path="sci/", target=target, band=band)
             aligned_images = align(unaligned_images, centroid=hybrid_centroid, filter="combined")
             stacked_image = stack(aligned_images, correct_exposure=True)
