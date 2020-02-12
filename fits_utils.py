d0853535b8c392c90dac51eb811df5783363f9d0
Alex <alex.s.wheaton@gmail.com>
2020-02-12 10:16:52 +0000

    Moved config generator inside main module.

--- fits_utils.py
+++ fits_utils.py
@@ -34,6 +34,16 @@ from astropy.io import fits
 from os import walk
 import configparser
 
+def gen_config():
+    config = configparser.ConfigParser()
+    config['TELESCOPE'] = {'Size': '15'}
+    config['DATA SETTINGS'] = {'Standard Star A': 'bd62',
+                               'Standard Star B': 'bd25',
+                               'Target ID': 'm52',
+                               'Bands': 'g, r, u'}
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+
 def get_lists(dir):
     """
     Sorts fits files into lists.
