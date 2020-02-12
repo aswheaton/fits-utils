1664a866f074d077c935f982d75ba1bf7bb8f70f
Alex Wheaton <alex.s.wheaton@gmail.com>
2020-02-12 11:58:57 +0000

    Reorganized some code.

--- stack.py
+++ stack.py
@@ -1,15 +1,21 @@
 """Script to align and stack images.
 
 """
-from fits_utils import *
+import fits_utils as fu
 
 def main():
     for target in ["m52"]:
-        for band in ["r","g","u"]:
-            unaligned_images = load_fits(path="sci/", target=target, band=band)
-            aligned_images = align(unaligned_images, centroid=hybrid_centroid, filter="combined")
-            stacked_image = stack(aligned_images, correct_exposure=True)
-            write_out_fits(stacked_image, "sta/{}_{}_stacked.fits".format(target, band))
+        for band in ["r","g"]:
+            unaligned_images = fu.load_fits(path="sci/", target=target, band=band)
+            aligned_images = fu.align(unaligned_images, centroid=hybrid_centroid, filter="none")
+            stacked_image = fu.stack(aligned_images, correct_exposure=True)
+            fu.write_out_fits(stacked_image, "sta/{}_{}_stacked.fits".format(target, band))
+    for target in ["m52"]:
+        for band in ["u"]:
+            unaligned_images = fu.load_fits(path="sci/", target=target, band=band)
+            aligned_images = fu.align(unaligned_images, centroid=hybrid_centroid, filter="combined")
+            stacked_image = fu.stack(aligned_images, correct_exposure=True)
+            fu.write_out_fits(stacked_image, "sta/{}_{}_stacked.fits".format(target, band))
 
 if __name__ == '__main__':
     main()
