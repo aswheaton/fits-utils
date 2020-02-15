Fixed error in chi-squared calculation.

de_redden.py
@@ -6,13 +6,16 @@ def main():
     A, B, C, D, E = 1.283, -0.107, -2.748, 8.477, -3.141
     pleiades_coeffs = [A, B, C, D, E]
     # Zero points from zero-point-calculator.
-    zpr, zpg, zpu = 1.0, 2.0, 3.0
+    zpr, zpg, zpu = 30.0, 30.0, 27.0
     # Get the zero point corrected catalog and error.
     r_mag, r_err, g_mag, g_err, u_mag, u_err = load_cat('cat/combined.cat', zpr, zpg, zpu)
     sqr_err = g_err**2 + r_err**2 + u_err**2
     # Calculate the colour excess.
     gr_excess = g_mag - r_mag
     ug_excess = u_mag - g_mag
+    # Calculate error on colour excess.
+    gr_excess_err = g_err + r_err
+    ug_excess_err = u_err + g_err
 
     plt.scatter(ug_excess,gr_excess)
     plt.title("Reddened U-V vs. G-R Colour Excess")
@@ -26,7 +29,9 @@ def main():
         for red_vec_y in np.linspace(-10.0, 10.0, 100):
             gr_excess_shifted = gr_excess + red_vec_x
             ug_excess_shifted = ug_excess + red_vec_y
-            chi_squ = get_chi_squ(gr_excess_shifted, ug_excess_shifted, polynomial, pleiades_coeffs)
+            chi_squ = get_chi_squ(gr_excess_shifted, ug_excess_shifted,
+                                  ug_excess_err, polynomial, pleiades_coeffs
+                                 )
             params_and_fit[row,0] = red_vec_x
             params_and_fit[row,1] = red_vec_y
             params_and_fit[row,2] = chi_squ
@@ -43,6 +48,7 @@ def main():
 
     plt.scatter(gr_excess, ug_excess)
     plt.scatter(de_reddened_gr_excess, de_reddened_ug_excess)
+    plt.scatter(gr_excess, polynomial(gr_excess, pleiades_coeffs))
     plt.title("Dereddened U-V vs. G-R Colour Excess")
     plt.show()
 

fits_utils.py
@@ -509,18 +509,13 @@ def polynomial(x, coeffs):
         y += coeffs[n] * x ** n
     return(y)
 
-def get_chi_squ(x_obs, y_obs, func, coeffs):
+def get_chi_squ(x_obs, y_obs, y_err, func, coeffs):
     """
     Returns the chi-squared value for a given x,y dataset and a function handle
     that returns the predicted predicted values.
     """
-    # chi_squ_tot = 0.0
-    # for i in range(len(x_obs)):
-    #     y_exp = func(x_obs[i], coeffs=coeffs)
-    #     chi_squ = (y_obs[i] - y_exp[i])**2 / y_exp[i]
-    #     chi_squ_tot += chi_squ
     y_exp = func(x_obs, coeffs)
-    chi_squ_tot = np.sum((y_obs-y_exp)**2 / y_exp)
+    chi_squ_tot = np.sum((y_obs-y_exp)**2 / y_err**2)
     return(chi_squ_tot)
 
 def minimiser_1(lambda_fit):