d570eb70e0b862541ecd4acea9910c54da72e1a3
Alex <alex.s.wheaton@gmail.com>
2020-02-17 09:39:11 +0000

    Removed legacy code.

--- de_redden.py
+++ de_redden.py
@@ -63,37 +63,4 @@ def main():
 
     write_cat(r_mag, de_reddened_g_mag, de_reddened_u_mag,
               "de_reddened_combined")
-
-    # m= 0.9199548
-    # ms=(m**2)+1
-    # delrelx= (1/ms)**0.5
-    # delrely=(m)/((ms)**0.5)
-    # a= -6.9834437936715
-    # b=-0.36562274114452
-    # c=1.3158896161408
-    # k2 = m+b
-    # ll= int(input("Please enter a lower limit for the reddening vector: "))
-    # ul= int(input("Please enter an upper limit for the reddening vector: "))
-    # dr= int(input("Please enter a step-size: "))
-
-    # lambda_fit = []
-    # for i in np.linspace(ll, ul, num=(ul-ll)/dr):
-    #     delta_x, delta_y = i * delrelx, i* delrely
-    #     chi_sqr = 0.0
-    #     correct_ug_excess = ug_excess + delta_y
-    #     correct_gr_excess = gr_excess - delta_x
-    #     k1 = (c+(correct_ug_excess)) - (m*correct_gr_excess)
-    #     #: Only care about negative solution.
-    #     ex_x = (k2-((k2**2)+(4*a*k1))**0.5)/(2*a)
-    #     ex_y = a*(ex_x**2) + b*ex_x + c
-    #     r_sq = (correct_gr_excess - ex_x)**2 + (correct_ug_excess - ex_y)**2
-    #     chi_sq = r_sq/sqr_err
-    #     fit = np.array([np.abs(i), np.sum(chi_sq)])
-    #     lambda_fit.append(fit)
-    # lambda_fit = np.array(lambda_fit)
-    # best_fit = minimiser(lambda_fit)
-    # print('Best a lambda: {}'.format(best_fit))
-    # de_red_ug = ug_excess + delrely * best_fit
-    # de_red_gr = gr_excess - delrelx * best_fit
-
 main()
